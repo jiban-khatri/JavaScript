36a66d76538136f4bae373b5a0c077ff9f0ae79e
Jiban Khatri <contact.jiban01@gmail.com>
2026-08-08 20:42:34 +0545

    Some Codes Pushed

---
+++ controlflow.js
@@ -0,0 +1,40 @@
+console.log("Learning Control Flow");
+
+/*
+if (true){
+    console.log("The condition is true");   // Condition satisfies; so, else statement doesn't executes
+} else {
+    console.log("Condition is False");
+}
+
+
+
+
+let age = 18;
+
+if (age >= 18) {
+    console.log("Eligible to vote");
+} else {
+    console.log("Not eligible to vote");
+}
+
+*/
+
+
+
+// IF, ELSE IF, ELSE
+
+let score = 91;
+
+if (score > 90) {
+    console.log("A")
+}
+else if (score > 80 && score <= 90) {
+    console.log("B")
+}
+else if (score > 70 && score <= 80){
+    console.log("C")
+}
+else {
+    console.log("Fail")
+}
---
+++ loops.js
@@ -0,0 +1,79 @@
+console.log("Loops and Iterations")
+
+/*
+Loops = Process of repeating block of code 
+Iterations = Looping : looping first time = 1st iteration ...........
+*/
+
+
+
+// for loop - loop best when we exactly know how many times we need to run a block of code
+
+/* for(initialization; condition; update){
+    // code
+} */
+
+// for (let i = 0; i <= 10; i+=1){
+//     console.log("iteration = ", i)
+// }
+
+
+// let language = "javascript"
+
+// for (let i=0; i< language.length; i++){
+//     console.log(language.charAt(i))
+// }
+
+
+
+
+
+// Nested Loop
+
+// for (let i=1; i<3; i++){
+//     for (let j=1; j<3; j++){
+//         console.log("Row",i, "Column",j)
+//     }
+// }
+
+
+
+
+
+
+
+// Break and Continue
+
+// Break stops the loop immediately 
+// for (let i = 0; i<=5; i++){
+//     if (i === 3){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// Continue skips the current iteration and continuous the next iteration
+
+// for (let i=1; i<=5; i++){
+//     if(i === 3){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
+// Multiple counters for single loop
+
+// for (let i=1, j=10; i<=10 && j>=1; i++, j--){
+//     console.log(i,j)
+// }
+
+
+
+
+
+
