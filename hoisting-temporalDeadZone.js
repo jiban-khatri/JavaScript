841ab8bb518b175d229f2adac0a3886c6a9ddf56
Jiban Khatri <contact.jiban01@gmail.com>
2026-08-12 22:41:11 +0545

    hoisting and tdz

---
+++ hoisting-temporalDeadZone.js
@@ -0,0 +1,30 @@
+// Hoisting = variable declarations are processed before the execution 
+
+console.log("UserName is ", userName);     // undefined 
+
+var userName;   // No Error with var : hoisted
+userName = "David";
+
+console.log("UserName is", userName);
+
+
+
+
+// Temporal Dead Zone (TDZ) =  An area where you cannot access a variable until it is initialized.
+
+// Reference Error
+
+{
+    // age variable's TDZ starts here
+    //
+    console.log(age);   // Reference Error
+    //
+    //
+    //
+    //
+    let age = 20;   // age variable's TDZ ends here
+    //
+    //
+}
+
+
