4767facdcc7b2a66aba1de040875d322fd3a74d0
Jiban Khatri <contact.jiban01@gmail.com>
2026-08-17 22:50:11 +0545

    js arrays

--- array.js
+++ array.js
@@ -142,3 +142,86 @@ const districts = ["Dng", "Chitwan", "Ktm"];
 
 const mergedLocation = [...cities, ...districts];
 console.log(mergedLocation);
+
+
+
+
+// length
+
+arr1 = [11,22,33];
+console.log(arr.length);
+
+
+// Remember : Array can hold elements upto (2^32 - 1)
+console.log(2**32 - 1); // 4294967295
+
+//
+arr1.length = 2; // it removes last element
+console.log(arr1); 
+
+arr1.length = 5; // 3 empty slots
+console.log(arr1)
+
+
+
+
+// >>>>>>>>>>>>>>> JavaScript Array Methods <<<<<<<<<<<<<<<<<<<<<<
+
+
+// concat() method => merge two or more arrays
+const first = [1,2,3];
+const second = [4,5,6];
+
+const merged = first.concat(second);
+console.log(merged);
+
+
+
+// join() method
+const emotions = ['☺️', '😅', '🤣', '😍']
+const joined = emotions.join("<=>");    // returns a string with separator
+console.log(joined);
+
+
+
+// fill() method => fills the array with static value
+// -- changes the original array
+
+const colors = ['red', 'blue', 'green'];
+colors.fill('pink');
+console.log(colors);
+
+
+// includes() = if the element present in the array then it returns true else false
+
+const names = ["Jiban", "Tom", "David"];
+console.log(names.includes("Tom")); // true
+console.log(names.includes("Dravid"));  // false
+
+
+
+// indexOf() => returns the first occurence index
+
+console.log(names.indexOf("Tom"));  // 1
+console.log(names.indexOf("Brown")); // -1 : if the element doesn't occurs
+
+
+
+// reverse() => reverses the position of elements
+// -- permanent change in original array
+console.log(names.reverse());
+
+
+
+// sort() => it sorts the element on ascending order
+// -- it converts the item into string first then sorts
+
+const fruits = ["Orange", "Apple", "Mango", "Banana"];
+fruits.sort();
+console.log(fruits);
+
+
+
+// splice()
+// splice(start, deleteCount, ClipboardItem, item1, item2 ,......)
+
