e693c1e1f3a710adc3e2edc089bd08bb0a875045
Jiban Khatri <contact.jiban01@gmail.com>
2026-08-29 19:15:38 +0545

    Quiz App

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -119,17 +119,20 @@ const quizData = [
   },
 ];
 
+// DOM Elements
+
 const form = document.getElementById("my-form");
 
 const userDiv = document.querySelector(".user");
 const userName = document.getElementById("user-name");
-const gameBox = document.querySelector(".game-box")
+const gameBox = document.querySelector(".game-box");
+const playerInput = document.getElementById("player-input-name");
 
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let playerName = e.target.childNodes[1].value;
+  let playerName = playerInput.value;
 
   form.reset();
   startGame(playerName);
@@ -137,13 +140,102 @@ form.addEventListener("submit", (e) => {
 
 // start game
 function startGame(playerName) {
+  userDiv.style.display = "flex";
+  userName.textContent = playerName;
 
-    userDiv.style.display = "flex";
-    userName.textContent = playerName;
+  form.style.display = "none";
+  gameBox.style.display = "flex";
+  
+  let randomIndex = Math.floor(Math.random() * quizData.length);
+  updateQuestion(randomIndex);
 
-    form.style.display = "none";
-    gameBox.style.display = "flex";
-};
-
+  updateTime();
+}
 
 // timer function
+
+let initialTime = 90;
+const timerElem = document.getElementById("timer-sec");
+timerElem.textContent = initialTime;
+
+function updateTime() {
+  const timer = setInterval(() => {
+    initialTime -= 1;
+    timerElem.textContent = initialTime;
+
+    if (initialTime <= 0) {
+      clearInterval(timer);
+      displayScore();
+    };
+  }, 1000);
+
+  
+}
+
+
+
+let questionSection, optionSection, submitButton;
+
+// update Questions function
+
+function updateQuestion(randomIndex) {
+  let currentQuestion = quizData[randomIndex];
+
+  // remove old one
+  questionSection?.remove();
+  optionSection?.remove();
+  submitButton?.remove();
+
+  // question section
+  questionSection = document.createElement("div");
+  questionSection.classList.add("question-section");
+
+  let h2 = document.createElement("h2");
+  h2.textContent = currentQuestion.question;
+
+  questionSection.appendChild(h2);
+
+  // options section
+  optionSection = document.createElement("div");
+  optionSection.classList.add("options-section");
+
+  currentQuestion.options.forEach((ans) => {
+    let p = document.createElement("p");
+    p.classList.add("options");
+    p.textContent = ans;
+
+    optionSection.appendChild(p);
+  });
+
+
+
+  // Next Button
+  submitButton = document.createElement("button");
+  submitButton.classList.add("next-qns-btn");
+  submitButton.textContent = "Next";
+
+  gameBox.append(questionSection, optionSection, submitButton);
+
+  submitButton.addEventListener("click", () => {
+    let randomIndex = Math.floor(Math.random() * quizData.length);
+    updateQuestion(randomIndex);
+  });
+}
+
+// 
+const outerDiv = document.querySelector(".outer");
+
+function displayScore() {
+  // form.style.display = "none";
+  // gameBox.style.display = "none";
+
+  outerDiv.innerHTML = `
+  <div class="score-section" style="display: flex; flex-direction: column; justify-content:center; align-items:center">
+        <h2 style="color: rgb(144, 0, 255); font-size: 30px;">Congratulations ! You Scored 10 Points.</h2>
+        <button class="play-again">Play Again</button>
+    </div>
+  `;
+}
+
+
+// validateAnswer(randomIndex);
